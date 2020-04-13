39985aaaa0618605d779e671082d5e95414e4311
Artem Bondarenko <artem.arturovich.bondarenko@gmail.com>
2020-04-13 08:40:28 +0300

    Updates

---
+++ precache-manifest.1cfea92d29592fb8b125f54604fe01ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "010c0357f7e5c72d38932564655f5bdd",
+    "url": "/ArtemArturovichBondarenko/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "b5cfdce145f9a65e6ef8",
+    "url": "/ArtemArturovichBondarenko/goit-react-hw-01-components/static/css/main.5826d478.chunk.css"
+  },
+  {
+    "revision": "2e3a62ce0aade59a19a0",
+    "url": "/ArtemArturovichBondarenko/goit-react-hw-01-components/static/js/2.1bf5fddc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ArtemArturovichBondarenko/goit-react-hw-01-components/static/js/2.1bf5fddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5cfdce145f9a65e6ef8",
+    "url": "/ArtemArturovichBondarenko/goit-react-hw-01-components/static/js/main.a8671fba.chunk.js"
+  },
+  {
+    "revision": "ae908b565f1d58a787f9",
+    "url": "/ArtemArturovichBondarenko/goit-react-hw-01-components/static/js/runtime-main.cf16da1b.js"
+  }
+]);
